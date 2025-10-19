0b648bfb587c00199c5be0c93178e33080a3d352
Ozair Khan <ozairkhan2810@gmail.com>
2025-10-19 13:04:18 +0530

    Update title | Name of project | remove trailing slash from url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // base path for GitHub Pages (owner: ozairkhan2810, repo: timespent)
-  base: '/timespent/',
+  base: '/timespent',
   plugins: [react()],
 })
